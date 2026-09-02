9dc2b0b2843eb65a5111f062be3bec22b0014414
25maae <eaa25maae@students.eaaa.dk>
2026-09-02 16:27:12 +0200

    Implement lazy loading for EventPage to enhance performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
-import EventPage from "./pages/EventPage";
+const EventPage = lazy(() => import("./pages/EventPage"));
 import RegistrationsPage from "./pages/RegistrationsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
@@ -13,7 +14,14 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/events/:eventId" element={<EventPage />} />
+        <Route
+          path="/events/:eventId"
+          element={
+            <Suspense fallback>
+              <EventPage />
+            </Suspense>
+          }
+        />
         <Route path="/om" element={<AboutPage />} />
         <Route path="/tilmeldinger" element={<RegistrationsPage />} />
         <Route path="*" element={<NotFoundPage />} />
