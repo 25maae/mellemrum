e8fd0d70d2e0013e0116feddba63c16549c9518f
25maae <eaa25maae@students.eaaa.dk>
2026-09-25 14:50:41 +0200

    Refactor ContactPage layout: change main to header and update content structure for improved clarity

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,17 +4,17 @@ export default function ContactPage() {
   usePageTitle("Kontakt");
   return (
     <>
-      <main className="contact-page">
-        <h1>Kontakt os</h1>
+      <header className="hero">
+        <p className="eyebrow">Kontakt os</p>
+        <h1>
+          Har du spørgsmål?
+        </h1>
+        <p class="hero-copy"></p>
         <p>
-          Har du spørgsmål, forslag eller andet, er du altid velkommen til at
-          kontakte os.
-        </p>
-        <p>
-          Du kan skrive til os på{" "}
+          Du er altid velkommen til at skrive til os på:{" "}
           <a href="mailto:hej@mellemrum.dk">hej@mellemrum.dk</a>.
         </p>
-      </main>
+      </header>
     </>
   );
 }
