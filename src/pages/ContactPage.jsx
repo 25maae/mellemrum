929b0d6005bcf3c606685524eba71add66006e3a
25maae <eaa25maae@students.eaaa.dk>
2026-09-25 15:10:06 +0200

    Fix ContactPage layout: update header structure and improve content formatting

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,5 @@
 import { usePageTitle } from "../hooks/usePageTitle";
+import { Link } from "react-router";
 
 export default function ContactPage() {
   usePageTitle("Kontakt");
@@ -6,13 +7,17 @@ export default function ContactPage() {
     <>
       <header className="hero">
         <p className="eyebrow">Kontakt os</p>
-        <h1>
-          Har du spørgsmål?
-        </h1>
-        <p class="hero-copy"></p>
-        <p>
+        <h1>Har du spørgsmål?</h1>
+        <p class="hero-copy">
           Du er altid velkommen til at skrive til os på:{" "}
           <a href="mailto:hej@mellemrum.dk">hej@mellemrum.dk</a>.
+        </p>
+        <p class="hero-copy">
+          Du kan også følge med på:{" "}
+          <Link to="/LinkedIn" className="contact-link">
+            LinkedIn
+          </Link>
+          .
         </p>
       </header>
     </>
